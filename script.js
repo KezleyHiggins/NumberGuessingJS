a0452ca2e5b001472adbde2454fd1b9709e964c3
Kezley Higgins <kezleyhiggins@gmail.com>
2022-01-24 14:33:24 +0200

    Initial Upload

---
+++ script.js
@@ -0,0 +1,124 @@
+"use strict";
+
+// Classes
+
+class ScoreTracker{
+    constructor(){
+        this.score = 20;
+        this.scoreDisplay = document.querySelector(".score");
+        this.UpdateScoreDisplay();
+
+        this.highScore = 0;
+        this.highScoreDisplay = document.querySelector(".highscore");
+        this.UpdateHighScoreDisplay();
+    }
+
+    UpdateScoreDisplay(){
+        this.scoreDisplay.textContent = this.score;
+    }
+
+    UpdateHighScoreDisplay(){
+        this.highScoreDisplay.textContent = this.highScore;
+    }
+
+    DecreaseScore(){
+        this.score--;
+        this.UpdateScoreDisplay();
+    }
+
+    ResetScore(){
+        this.score = 20;
+        this.UpdateScoreDisplay();
+    }
+
+    UpdateHighScore(){
+        if(this.score > this.highScore){
+            this.highScore = this.score;
+            this.UpdateHighScoreDisplay();
+        }
+    }
+}
+
+// Variables
+
+const body = document.querySelector("body");
+const message = document.querySelector(".message");
+const numberDisplayer = document.querySelector(".number");
+const scoreTracker = new ScoreTracker();
+let canPlay = true;
+let hasWon = false;
+let hasLost = false;
+let secretNumber = Math.trunc(Math.random()*20)+1;
+
+// Functions
+
+function OnGuessClick() {
+    if(canPlay == false) return;
+
+    const guess = Number(document.querySelector(".guess").value);    
+
+    if(!guess){
+      message.textContent = "🛑 No Number!"
+    }
+    else if(guess === secretNumber){
+      message.textContent = "🎉 Correct!"
+      numberDisplayer.textContent = secretNumber;
+      scoreTracker.UpdateHighScore();
+      canPlay = false;
+      hasWon = true;
+    }
+    else if(guess > secretNumber){
+      message.textContent = "👆🏻 Too high!"
+      scoreTracker.DecreaseScore();
+    }
+    else if(guess < secretNumber){
+      message.textContent = "👇🏻 Too low!"
+      scoreTracker.DecreaseScore();
+    }
+    
+    if(scoreTracker.score < 1){
+        canPlay = false;
+        hasLost = true;
+        console.log(hasLost);
+        message.textContent = "💥 You lost!"
+    }
+}
+
+function OnAgainClick(){
+    document.querySelector(".guess").value = "";
+    scoreTracker.ResetScore();
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    numberDisplayer.textContent = "?";
+    canPlay = true;
+    hasWon = false;
+    hasLost = false;
+}
+
+function UpdateVisuals(){
+    if(canPlay == true){
+        document.querySelector("#checkButton").style.backgroundColor = "#eee"
+    }
+    else{
+        document.querySelector("#checkButton").style.backgroundColor = "#8f8c8c"
+    }
+
+    if(hasWon === true){
+        body.style.backgroundColor = "#54b811"
+    }
+    else if(hasLost === true){
+        body.style.backgroundColor = "#b81111"
+    }
+    else{
+        body.style.backgroundColor = "#222"
+    }
+}
+
+// Main Code
+
+document.querySelector(".check").addEventListener("click", function()
+{ OnGuessClick(); UpdateVisuals(); });
+
+document.querySelector(".again").addEventListener("click", function()
+{ OnAgainClick(); UpdateVisuals(); });
+
+numberDisplayer.textContent = "?";
